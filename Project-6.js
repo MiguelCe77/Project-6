88d33509edf529e78795b1c6fabbd9f89faf4c66
Miguel Ceballos <ceballosmiguel777@gmail.com>
2016-11-29 08:46:25 -0800

    Added kinda fuctioning overlap

--- Project-6.js
+++ Project-6.js
@@ -1,26 +1,36 @@
-var batx = 200;
-var baty = 100;
+var playerX = 200;
+var playerY = 100;
 
 document.addEventListener("keydown", function(e) {
 
+
   if(e.keyCode == 37){
-    batx -=15;
-    document.getElementById("bat").setAttribute("x", batx)
+    playerX -=15;
+    document.getElementById("bat").setAttribute("x", playerX)
 
   }
 
   else if(e.keyCode == 39){
-    batx +=15;
-    document.getElementById("bat").setAttribute("x", batx)
+    playerX +=15;
+    document.getElementById("bat").setAttribute("x", playerX)
   }
 
   else if(e.keyCode == 40){
-    baty +=15;
-    document.getElementById("bat").setAttribute("y", baty)
+    playerY +=15;
+    document.getElementById("bat").setAttribute("y", playerY)
   }
 
   else if(e.keyCode == 38){
-    baty -=15;
-    document.getElementById("bat").setAttribute("y", baty)
+    playerY -=15;
+    document.getElementById("bat").setAttribute("y", playerY)
+
   }
+
+    foodY = Number(document.getElementById("food1").getAtrribute("y"))
+    foodX = Number(document.getElementById("food1").getAtrribute("x"))
+
+    if (playerX > foodX && playerX < foodX + width && playerY > foodY && playerY < foodY + hieght) {
+        console.log("hello")
+    }
+
 })
